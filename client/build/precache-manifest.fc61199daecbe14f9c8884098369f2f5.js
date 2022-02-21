eff0be18fbda45b96276282a53676070f6acd8e9
Paul McDevitt <paul.mcd1@icloud.com>
2022-02-21 18:09:52 -0500

    adding files

---
+++ client/build/precache-manifest.fc61199daecbe14f9c8884098369f2f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e9dc6e14ecb346a8b6599bb40ce8fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8193e2734db10b1a48",
+    "url": "/static/css/main.d286ef19.chunk.css"
+  },
+  {
+    "revision": "3c56317cc20c456c207c",
+    "url": "/static/js/2.1126382f.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.1126382f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8193e2734db10b1a48",
+    "url": "/static/js/main.e63dee75.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
